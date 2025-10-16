f88dbf40d2e22103e708f06e504fa934dbfe7baf
shadeyxr <89664101+shadeyxr@users.noreply.github.com>
2025-10-16 17:45:22 +0100

    Add team shuffling

--- champions-league-game/src/Brackets.tsx
+++ champions-league-game/src/Brackets.tsx
@@ -1,4 +1,44 @@
+import {useState, useEffect} from 'react'
+
 export default function Brackets() {
+  const teams = [
+    "Liverpool",
+    "Barcelona",
+    "Arsenal",
+    "Inter Milan",
+    "Atlético Madrid",
+    "Bayer Leverkusen",
+    "Lille",
+    "Aston Villa",
+    "Borussia Dortmund",
+    "Bayern Munich",
+    "Benfica",
+    "Club Brugge",
+    "Feyenoord",
+    "Paris Saint-Germain",
+    "Real Madrid",
+    "PSV Eindhoven"
+  ];
+
+  const [pairsLeft, setPairsLeft] = useState<[string, string][]>([]);
+  const [pairsRight, setPairsRight] = useState<[string, string][]>([]);
+
+  useEffect(()=>{
+    const shuffled = [...teams].sort(()=> 0.5 - Math.random());
+    const left: [string,string][] = [];
+    const right: [string,string][] = [];
+    for (let i = 0; i < 8; i+=2){
+      left.push([shuffled[i], shuffled[i+1]])
+    }
+    for (let i = 8; i < 16; i+=2){
+      right.push([shuffled[i], shuffled[i+1]])
+    }
+
+    setPairsLeft(left)
+    setPairsRight(right)
+    
+  },[])
+
   return (
     <div className="flex justify-center items-stretch gap-50 ">
       {/* Left side */}
@@ -6,63 +46,29 @@ export default function Brackets() {
         {/* Round of 16 */}
         <div className="flex flex-col gap-8 justify-center">
           <h2 className="text-lg font-bold text-center text-white">Round of 16</h2>
-          <div className="p-3 border rounded bg-gray-100 flex justify-center gap-4 w-32">
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
-              </button>
-            </div>
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./barcelona-badge.png" className="w-8 h-8 mt-1" alt="Barcelona Badge" />
-              </button>
-            </div>
-          </div>
+          {pairsLeft.map(([team1, team2]: [string, string]) => {
 
-          <div className="p-3 border rounded bg-gray-100 flex justify-center gap-4 w-32">
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
-              </button>
-            </div>
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./barcelona-badge.png" className="w-8 h-8 mt-1" alt="Barcelona Badge" />
-              </button>
-            </div>
-          </div>
-
-          <div className="p-3 border rounded bg-gray-100 flex justify-center gap-4 w-32">
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
-              </button>
-            </div>
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./barcelona-badge.png" className="w-8 h-8 mt-1" alt="Barcelona Badge" />
-              </button>
-            </div>
-          </div>
-
-          <div className="p-3 border rounded bg-gray-100 flex justify-center gap-4 w-32">
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
-              </button>
-            </div>
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./barcelona-badge.png" className="w-8 h-8 mt-1" alt="Barcelona Badge" />
-              </button>
-            </div>
-          </div>
+            return (
+              <div className="p-3 border border-black rounded bg-gray-100 flex justify-center gap-4 w-32" key={`${team1}-${team2}`}>
+                <div className="flex items-center" id={team1}>
+                  <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
+                    <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
+                  </button>
+                </div>
+                <div className="flex items-center" id={team2}>
+                  <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
+                    <img src="./barcelona-badge.png" className="w-8 h-8 mt-1" alt="Barcelona Badge" />
+                  </button>
+                </div>
+              </div>
+            )
+          })}
         </div>
 
         {/* Quarter finals */}
         <div className="flex flex-col gap-8 justify-center">
           <h2 className="text-lg font-bold text-center text-white">Quarter finals</h2>
-          <div className="p-3 border rounded bg-gray-100 flex justify-center gap-4 w-32">
+          <div className="p-3 border border-black rounded bg-gray-100 flex justify-center gap-4 w-32">
             <div className="flex items-center">
               <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
                 <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
@@ -75,7 +81,7 @@ export default function Brackets() {
             </div>
           </div>
 
-          <div className="p-3 border rounded bg-gray-100 flex justify-center gap-4 w-32">
+          <div className="p-3 border border-black rounded bg-gray-100 flex justify-center gap-4 w-32">
             <div className="flex items-center">
               <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
                 <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
@@ -92,7 +98,7 @@ export default function Brackets() {
         {/* Semi finals */}
         <div className="flex flex-col gap-8 justify-center">
           <h2 className="text-lg font-bold text-center text-white">Semi finals</h2>
-          <div className="p-3 border rounded bg-gray-100 flex justify-center gap-4 w-32">
+          <div className="p-3 border border-black rounded bg-gray-100 flex justify-center gap-4 w-32">
             <div className="flex items-center">
               <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
                 <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
@@ -105,8 +111,6 @@ export default function Brackets() {
             </div>
           </div>
         </div>
-
-
       </div>
 
       {/*middle*/}
@@ -114,7 +118,7 @@ export default function Brackets() {
       <div className="flex justify-start items-stretch gap-20 min-h-screen">
         <div className="flex flex-col gap-8 justify-center">
           <h2 className="text-lg font-bold text-center text-white">Final</h2>
-          <div className="p-3 border rounded bg-gray-100 flex justify-center gap-4 w-32">
+          <div className="p-3 border border-black rounded bg-gray-100 flex justify-center gap-4 w-32">
             <div className="flex items-center">
               <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
                 <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
@@ -128,70 +132,35 @@ export default function Brackets() {
           </div>
         </div>
       </div>
-
 
       {/* Right side */}
       <div className="flex flex-row-reverse justify-end items-stretch gap-20 min-h-screen">
         {/* Round of 16 */}
         <div className="flex flex-col gap-8 justify-center">
           <h2 className="text-lg font-bold text-center text-white">Round of 16</h2>
-          <div className="p-3 border rounded bg-gray-100 flex justify-center gap-4 w-32">
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
-              </button>
-            </div>
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./barcelona-badge.png" className="w-8 h-8 mt-1" alt="Barcelona Badge" />
-              </button>
-            </div>
-          </div>
+          {pairsRight.map(([team1, team2]: [string, string]) => {
 
-          <div className="p-3 border rounded bg-gray-100 flex justify-center gap-4 w-32">
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
-              </button>
-            </div>
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./barcelona-badge.png" className="w-8 h-8 mt-1" alt="Barcelona Badge" />
-              </button>
-            </div>
-          </div>
-
-          <div className="p-3 border rounded bg-gray-100 flex justify-center gap-4 w-32">
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
-              </button>
-            </div>
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./barcelona-badge.png" className="w-8 h-8 mt-1" alt="Barcelona Badge" />
-              </button>
-            </div>
-          </div>
-
-          <div className="p-3 border rounded bg-gray-100 flex justify-center gap-4 w-32">
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
-              </button>
-            </div>
-            <div className="flex items-center">
-              <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
-                <img src="./barcelona-badge.png" className="w-8 h-8 mt-1" alt="Barcelona Badge" />
-              </button>
-            </div>
-          </div>
+            return (
+              <div className="p-3 border border-black rounded bg-gray-100 flex justify-center gap-4 w-32" key={`${team1}-${team2}`}>
+                <div className="flex items-center" id={team1}>
+                  <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
+                    <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
+                  </button>
+                </div>
+                <div className="flex items-center" id={team2}>
+                  <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
+                    <img src="./barcelona-badge.png" className="w-8 h-8 mt-1" alt="Barcelona Badge" />
+                  </button>
+                </div>
+              </div>
+            )
+          })}
         </div>
 
         {/* Quarter finals */}
         <div className="flex flex-col gap-8 justify-center">
           <h2 className="text-lg font-bold text-center text-white">Quarter finals</h2>
-          <div className="p-3 border rounded bg-gray-100 flex justify-center gap-4 w-32">
+          <div className="p-3 border border-black rounded bg-gray-100 flex justify-center gap-4 w-32">
             <div className="flex items-center">
               <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
                 <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
@@ -204,7 +173,7 @@ export default function Brackets() {
             </div>
           </div>
 
-          <div className="p-3 border rounded bg-gray-100 flex justify-center gap-4 w-32">
+          <div className="p-3 border border-black rounded bg-gray-100 flex justify-center gap-4 w-32">
             <div className="flex items-center">
               <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
                 <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
@@ -221,7 +190,7 @@ export default function Brackets() {
         {/* Semi finals */}
         <div className="flex flex-col gap-8 justify-center">
           <h2 className="text-lg font-bold text-center text-white">Semi finals</h2>
-          <div className="p-3 border rounded bg-gray-100 flex justify-center gap-4 w-32">
+          <div className="p-3 border border-black rounded bg-gray-100 flex justify-center gap-4 w-32">
             <div className="flex items-center">
               <button className="focus:outline-none hover:scale-110 hover:bg-gray-200 transition-transform duration-200 rounded">
                 <img src="./mancity-badge.png" className="w-8 h-8" alt="Manchester City Badge" />
